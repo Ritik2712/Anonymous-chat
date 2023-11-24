f1783aad79377f7e9ada1176d92e35bc28e432ea
Ritik2712 <ritik27122mahajan@gmail.com>
2023-11-24 18:08:03 +0530

    added rooms and saving chat

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,24 @@
 const express = require("express");
-const {
-  MongoClient,
-  MongoClientOptions,
-  ServerApiVersion,
-} = require("mongodb");
 const cors = require("cors");
 const { Server } = require("socket.io"); // Add this
 http = require("http");
 require("dotenv").config();
 
+const { addMessage, generateResponse } = require("./Utils/funcions");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
-const client = new MongoClient(process.env.DB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+app.use("/api/", require("./routes/User"));
+app.use("/api/chat/", require("./routes/Chat"));
 
 const io = new Server(server, {
   cors: {
@@ -30,45 +27,29 @@ const io = new Server(server, {
   },
 });
 
-// Add this
-// Listen for when the client connects via socket.io-client
 io.on("connection", (socket) => {
-  console.log(`User connected ${socket.id}`);
-  socket.emit("message", "welcome to chatApp", socket.id);
-  // We can write our socket event listeners in here...
-  socket.broadcast.emit("message", "A user has joined the chat", socket.id);
-  socket.on("send", (message) => {
-    io.emit("message", message, socket.id);
+  socket.on("send", async (message, id) => {
+    console.log(id);
+    io.to(id).emit("message", message, socket.id, true);
+    addMessage(message, id);
+    const response = await generateResponse(message, id);
+    io.to(id).emit("message", response, socket.id, false);
   });
+
+  socket.on("join-room", (id) => {
+    socket.join(id);
+  });
+
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat", socket.id);
+    console.log("user Disconnect");
   });
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    return await client.db(process.env.DB_NAME).command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    console.log("closing");
-    // await client.close();
-  }
-}
-run()
-  .then(async (res) => {
-    console.log(res);
-    const db = client.db("test");
-    // await db.createCollection("SSSIT");
-    const col = await db.listCollections().toArray();
-    const coll = await db.collection("users");
-    let resp = await coll.find().toArray();
-    console.log(col);
-  })
-  .catch(console.dir);
-server.listen(PORT, () => "Server is running on port 3000");
+const dbo = require("./conn");
+server.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,22 +1,130 @@
-import React, { useRef, useState } from "react";
+import axios from "axios";
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Apps = ({ socket }) => {
   const [messages, setMessages] = useState([]);
   const [sms, setSms] = useState("");
+  const [chats, setChats] = useState([]);
+  const [roomName, setRoomName] = useState("");
+  const [index, setIndex] = useState(0);
+  const [typing, setTyping] = useState(false);
+  const [dots, setDots] = useState("...");
   const input = useRef(null);
+  const { id } = useParams();
+  const token = localStorage.getItem("token");
+  const apiUrl = process.env.REACT_APP_BACKEND;
+  var interval = null;
+  const getChats = () => {
+    axios({
+      method: "GET",
+      url: `${apiUrl}api/chat/get`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(async (res) => {
+        await setChats(res.data);
+        console.log(res.data);
+        var correct = false;
+        res.data.forEach((element, index) => {
+          if (element._id === id) {
+            correct = true;
+            setRoomName(element.name);
+            setMessages(element.messages);
+            setIndex(index);
+          }
+        });
+        if (id === undefined) {
+          navigate(`/${res.data[0]._id}`);
+          setRoomName(res.data[0].name);
+          setMessages(res.data[0].messages);
+          setIndex(0);
+          return;
+        }
+        if (!correct) {
+          alert("wrong chat link");
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        console.log(e.response);
+        if (e?.response?.status === 400 || e?.response?.status === 404) {
+          localStorage.removeItem("token");
+          navigate("/login");
+        } else {
+          alert("Server error");
+        }
+      });
+  };
 
-  socket.on("message", (message, id) => {
-    console.log(messages, "newMessages");
+  useEffect(() => {
+    getChats();
+  }, []);
+
+  useEffect(() => {
+    socket.emit("join-room", id);
+  }, [id]);
+
+  socket.on("message", (message, id, typing) => {
     var newMessage = [...messages];
     newMessage.push(message);
-    console.log("new Messages", newMessage);
+    setTyping(typing);
     setMessages(newMessage);
   });
   const sendMessage = (e) => {
+    if (typing || sms.length === 0) {
+      input.current.focus();
+      return;
+    }
     e.preventDefault();
-    socket.emit("send", sms);
+    console.log(id);
+    socket.emit("send", sms, id);
     setSms("");
     input.current.focus();
+  };
+
+  const createChat = async () => {
+    axios({
+      method: "post",
+      url: `${apiUrl}api/chat/add/`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        number: chats.length + 1,
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        var newChats = chats;
+        newChats.push(res.data);
+        setChats(newChats);
+        changeChat(res.data, newChats.length - 1);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const navigate = useNavigate();
+
+  const Logout = () => {
+    navigate("/login");
+    localStorage.removeItem("token");
+  };
+
+  const changeChat = (newChat, newIndex) => {
+    var newChats = [...chats];
+    console.log(newChats[index]);
+    newChats[index].messages = messages ?? [];
+    console.log(newChats, "newChats");
+    setIndex(newIndex);
+    setChats(newChats);
+    setRoomName(newChat.name);
+    setMessages(newChat.messages);
+    navigate(`/${newChat._id}`);
+    socket.emit("join-room", id);
   };
 
   return (
@@ -25,44 +133,46 @@ const Apps = ({ socket }) => {
         <h1>
           <i className="fas fa-smile"></i> ChatCord
         </h1>
-        <a href="index.html" className="btn">
-          Leave Room
-        </a>
+        <button onClick={Logout} className="btn">
+          Leave Chat
+        </button>
       </header>
       <main className="chat-main">
         <div className="chat-sidebar">
-          <h3>
-            <i className="fas fa-comments"></i> Room Name:
-          </h3>
-          <h2 id="room-name">JavaScript</h2>
-          <h3>
-            <i className="fas fa-users"></i> Users
-          </h3>
-          <ul id="users">
-            <li>Brad</li>
-            <li>John</li>
-            <li>Mary</li>
-            <li>Paul</li>
-            <li>Mike</li>
-            <li>Mike</li>
-            <li>Mike</li>
-            <li>Mike</li>
-            <li>Mike</li>
-            <li>Mike</li>
-            <li>Mike</li>
+          <h3>Chat Name:</h3>
+          <h2 id="room-name">{roomName}</h2>
+          <div className="chat-creator" onClick={createChat}>
+            <h3>
+              <i className="fas fa-comments"></i> Chats
+            </h3>
+            <h3>
+              <i className="fas fa-plus"></i>
+            </h3>
+          </div>
+          <ul id="chats">
+            {chats.map((item, index) => {
+              return (
+                <li
+                  className="chat"
+                  key={index}
+                  onClick={() => changeChat(item, index)}
+                >
+                  {item.name}
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className="chat-messages">
           {messages.map((item, index) => {
             return (
               <div className="message" key={index}>
-                <p className="meta">
-                  Brad <span>9:12pm</span>
-                </p>
+                <p className="meta">{index % 2 ? "ChatCord" : "Me"}</p>
                 <p className="text">{item}</p>
               </div>
             );
           })}
+          {typing ? <h6 className="typing">typing{dots}</h6> : null}
         </div>
       </main>
       <div className="chat-form-container">
@@ -75,13 +185,13 @@ const Apps = ({ socket }) => {
             ref={input}
             value={sms}
             onChange={(e) => {
-              console.log(messages);
               setSms(e.target.value);
             }}
             autoComplete="off"
           />
           <button className="btn" onClick={sendMessage}>
-            <i className="fas fa-paper-plane"></i> Send
+            <i className="fas fa-paper-plane"></i>{" "}
+            <span className="btn-txt">Send</span>
           </button>
         </form>
       </div>
